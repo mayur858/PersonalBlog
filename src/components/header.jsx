3fc982ebc795609da38d2bdb8d5d9fb925e91a62
Mayur <kmayur819@gmail.com>
2025-02-09 21:12:38 +0530

    updated a tag with link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,9 +4,9 @@ function Header() {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-100 fixed top-0 w-full">
       <div className="ml-auto">
-        <a href="/about" className="text-gray-800 font-bold">
+        <link href="/about-page" className="text-gray-800 font-bold">
           About
-        </a>
+        </link>
       </div>
     </header>
   );
