2a98744bda112bc8cda451d0c123feaa506d7835
Mayur <kmayur819@gmail.com>
2025-02-09 21:18:12 +0530

    bug fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-100 fixed top-0 w-full">
       <div className="ml-auto">
-        <link href="/about-page" className="text-gray-800 font-bold">
+        <Link to="/about-page" className="text-gray-800 font-bold">
           About
-        </link>
+        </Link>
       </div>
     </header>
   );
